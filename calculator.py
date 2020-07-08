5795ace258093845267b8ead876cbe4ac02f23ff
Stephanie Resis <sresis@umich.edu>
2020-07-08 22:56:59 +0000

    initial commit

---
+++ calculator.py
@@ -0,0 +1,20 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+def main():
+
+	while True:
+		input_string = input()
+		tokens = input_string.split()
+		if tokens[0] == '+':
+			answer = add(int(tokens[1]), int(tokens[2]))
+			print(answer)
+
+
+
+main()
+
+
+
+# Replace this with your code
