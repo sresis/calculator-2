65c1b7c371bd676090a6a54e3e6bcef01066a164
Stephanie Resis <sresis@umich.edu>
2020-07-08 23:03:57 +0000

    implementing multiply function

--- calculator.py
+++ calculator.py
@@ -10,9 +10,13 @@ def main():
 		if tokens[0] == '+':
 			answer = add(int(tokens[1]), int(tokens[2]))
 			print(answer)
-		elif tokens[0] == '-'
+		elif tokens[0] == '-':
 			answer = subtract(int(tokens[1]), int(tokens[2]))
 			print(answer)
+		elif tokens[0] == '*':
+			answer = multiply(int(tokens[1]), int(tokens[2]))
+			print(answer)
+
 
 
 
