b61e3078c9063ea416e8c92e7696d243ede1ae5b
Stephanie Resis <sresis@umich.edu>
2020-07-09 01:11:35 +0000

    added functionality ffor input validation and infinite inputs (for addition only so far)

--- calculator.py
+++ calculator.py
@@ -2,18 +2,34 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
-def main():
+from functools import reduce
 
+#function to determine if the number inputs are valid
+def valid_input_check(nums):
+	for num in nums:
+		if not num.isdigit():
+			return False
+	return True
+
+def main():
+	# test
+	print(reduce(subtract, [1, 2, 3, 4]))
+	#main function to perform calculations
 	while True:
 		input_string = input()
 		tokens = input_string.split()
 		operator, *nums = tokens
 		#checks to make sure operator is valid
 		approved_operators = ['+', '-', '*', '/', 'square', 'cube', 'power', 'mod', 'q']
-		if operator in approved_operators:
+		# operation to convert each num in nums to a float
+		modified_nums = []
+		for num in nums:
+			modified_nums.append(float(num))
+		# determine if nums input is not valid (is not a digit)
+		if operator in approved_operators and valid_input_check(nums):
 		# can put print function at end instead of in each if statement
 			if operator == '+':
-				answer = add(int(tokens[1]), int(tokens[2]))
+				answer = reduce(add, modified_nums)
 				print(answer)
 			elif operator == '-':
 				answer = subtract(int(tokens[1]), int(tokens[2]))
@@ -40,7 +56,7 @@ def main():
 				return
 		# if operator is not valid, print failure message
 		else:
-			print('Invalid operation. Please try again')
+			print('Invalid input. Please try again')
 
 
 
